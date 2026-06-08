44b266030c7fd5babc20a34b62846f350e500ed2
q <jam@jam06452.uk>
2026-06-08 13:15:02 +0100

    Feature: OAuth table for OAuth

---
+++ priv/repo/migrations/20260608121125_oauth.exs
@@ -0,0 +1,14 @@
+defmodule Sancha.Repo.Migrations.Oauth do
+  use Ecto.Migration
+
+  def change do
+    create table(:oauth, primary_key: false) do
+      add :uuid, :uuid, null: false
+      add :name, :string
+      add :email, :string
+      add :slack_id, :string
+      add :avatar, :string
+      add :provider, :string
+    end
+  end
+end
