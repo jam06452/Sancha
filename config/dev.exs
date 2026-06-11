7fe03af525170b50c33b1bbb5595a324ab7d173b
jam06452 <danjward8@gmail.com>
2026-06-11 20:31:13 +0100

    Changed from 127.0.0.1 to localhost for cookie issues

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :sancha, Sancha.Repo,
 config :sancha, SanchaWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  url: [host: "localhost"],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
